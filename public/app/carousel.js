6e5375ce97b8e93b42f8f1115c631807afc08891
Iova Andreia <iova_andreia@yahoo.com>
2016-10-28 14:28:01 +0300

    Add progress icons

--- public/app/carousel.js
+++ public/app/carousel.js
@@ -2,23 +2,40 @@ var currentIndex = 0,
   items = $('.container-carousel div'),
   itemAmt = items.length;
 
-function cycleItems() {
-  var item = $('.container-carousel div').eq(currentIndex);
-  items.hide();
-  item.css('display','inline-block');
+  items.each(addProgress);
+
+
+function addProgress(){
+  $('.progress-slider').append('<div class="progress"></div>');
 }
 
-var autoSlide = setInterval(function() {
+var itemsProgress = $('.progress-slider div');
+
+
+
+function cycleItems() {
+  var item = $('.container-carousel div').eq(currentIndex);
+  var itemProgress = $('.progress-slider div').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+  itemsProgress.css('background-color','transparent');
+  itemProgress.css('background-color','white');
+}
+
+(function Slide(){
+  var autoSlide = setInterval(function() {
   console.log('in functie');
   currentIndex += 1;
   if (currentIndex > itemAmt - 1) {
     currentIndex = 0;
   }
   cycleItems();
-}, 3000);
+  }, 3000);
+})();
+
 
 $('.next').click(function() {
-  clearInterval(autoSlide);
+  //clearInterval(autoSlide);
   currentIndex += 1;
   if (currentIndex > itemAmt - 1) {
     currentIndex = 0;
@@ -27,7 +44,7 @@ $('.next').click(function() {
 });
 
 $('.prev').click(function() {
-  clearInterval(autoSlide);
+  //clearInterval(autoSlide);
   currentIndex -= 1;
   if (currentIndex < 0) {
     currentIndex = itemAmt - 1;
