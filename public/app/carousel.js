113081d105ff71782a529ee2c5b9a0e5b84cf0c8
Iova Andreia <iova_andreia@yahoo.com>
2016-10-31 15:48:48 +0200

    Add dragstart and dragend events

--- public/app/carousel.js
+++ public/app/carousel.js
@@ -1,16 +1,4 @@
-var localtunnel = require('localtunnel');
 
-var tunnel = localtunnel(port, function(err, tunnel) {
-    if (err) ...
-
-    // the assigned public url for your tunnel
-    // i.e. https://abcdefgjhij.localtunnel.me
-    tunnel.url;
-});
-
-tunnel.on('close', function() {
-    // tunnels are closed
-});
 
 
 var currentIndex = 0,
